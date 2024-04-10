afbe228a3781591b85f4b2be4338a343857382e7
GaboAfk <gabrielmorffe95@gmail.com>
2024-04-10 14:03:03 -0300

    quick push

---
+++ 3-quick_sort.c
@@ -0,0 +1,17 @@
+/*
+* File: 3-quick_sort.c
+* Auth: Gabriel Morffe,Agustin Rodriguez
+*/
+
+#include "sort.h"
+
+/**
+ * quick_sort - function that sorts an array of integers in ascending order using the Quick sort algorithm
+ *
+ * Return: Always 0 (Success)
+ */
+void quick_sort(int *array, size_t size)
+{
+	/*yourcodebitch*/
+		return (0);
+}
